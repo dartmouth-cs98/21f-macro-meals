232eebf68c4c012e584dbca0321824ae5703afbe
Quinn Spraut <quinn.a.spraut.22@dartmouth.edu>
2021-11-14 12:39:43 -0500

    form functionality added

--- macro-frontend/src/screens/MainScreen.js
+++ macro-frontend/src/screens/MainScreen.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
-  Text, View, TouchableOpacity, Dimensions,
+  Text, View, TouchableOpacity, Dimensions, TextInput, Picker
 } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect, useDispatch } from 'react-redux';
@@ -12,32 +12,22 @@ import { userLogout } from '../redux/actions/userActions';
 import styles from '../styles';
 
 const windowWidth = Dimensions.get('window').width;
-
-const uploadImageToS3 = (file) => {
-  console.log(file);
-  uploadImage(file).then((url) => {
-    classifyImage(url);
-  }).catch((error) => {
-    alert(error);
-  });
-};
-
-const classifyImage = (url) => {
-  axios.post('https://macro-cs98.herokuapp.com/api/classifyImage', {
-    url,
-  })
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
-};
+const windowHeight = Dimensions.get('window').height;
 
 function MainScreen({ navigation, storedUserName }) {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [cameraRef, setCameraRef] = useState(null);
+  const [showForm, setShowForm] = useState(false);
+  const [customName, setCustomName] = useState('');
+  const [mealTime, setMealTime] = useState('breakfast');
+  const [mood, setMood] = useState('positive');
+  const [imageUrl, setImageUrl] = useState('');
+  const [classification, setClassification] = useState('');
+  const [calories, setCalories] = useState(0);
+  const [protein, setProtein] = useState(0);
+  const [carb, setCarb] = useState(0);
+  const [fat, setFat] = useState(0);
 
   useEffect(() => {
     (async () => {
@@ -49,25 +39,71 @@ function MainScreen({ navigation, storedUserName }) {
 
   // eslint-disable-next-line no-unused-vars
   const handleLogout = () => {
-    console.log('logout');
     dispatch(userLogout());
     navigation.navigate('Logout');
   };
 
+  const uploadImageToS3 = (file) => {
+    uploadImage(file).then((url) => {
+      setImageUrl(url);
+      classifyImage(url);
+    }).catch((error) => {
+      alert(error);
+    });
+  };
+
+  const classifyImage = (url) => {
+    axios.post('https://macro-cs98.herokuapp.com/api/classifyImage', {
+      url,
+    })
+      .then((response) => {
+        console.log(response.data);
+        setClassification(response.data.classification);
+        setCalories(response.data.calories);
+        setProtein(response.data.protein);
+        setCarb(response.data.carb);
+        setFat(response.data.fat);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   async function onPictureSaved(photo) {
-    console.log(photo);
+    cameraRef.pausePreview();
     const response = await fetch(photo.uri);
     const blob = await response.blob();
+    setShowForm(true);
     uploadImageToS3(blob);
   }
 
   const takePicture = () => {
-    console.log('taking picture');
-    console.log(cameraRef);
     if (cameraRef) {
       cameraRef.takePictureAsync({ onPictureSaved });
     }
   };
+
+  const submitForm = () => {
+    console.log('form submit');
+    axios.post('https://macro-cs98.herokuapp.com/api/saveFood', {
+      username: storedUserName,
+      customName,
+      mealTime,
+      mood,
+      imageUrl,
+      classification,
+      calories,
+      protein,
+      carb,
+      fat,
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
 
   return (
     <View style={styles.container}>
@@ -79,6 +115,8 @@ function MainScreen({ navigation, storedUserName }) {
         type={type}
         ref={(ref) => { setCameraRef(ref); }}
       >
+        {!showForm
+        && (
         <View style={{
           width: '100%',
           height: '100%',
@@ -134,6 +172,54 @@ function MainScreen({ navigation, storedUserName }) {
             </TouchableOpacity>
           </View>
         </View>
+        )}
+        {showForm
+        && (
+        <View style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          position: 'relative',
+          paddingTop: 0.25 * windowHeight,
+          paddingBottom: 0.25 * windowHeight,
+        }}
+        >
+          <TextInput
+          style={styles.textInput}
+          onChangeText={setCustomName}
+          value={customName}
+          placeholder="custom name (ie. chicken breast)"
+          />
+          <Picker
+            selectedValue={mealTime}
+            onValueChange={(itemValue, itemIndex) => setMealTime(itemValue)}
+          >
+            <Picker.Item label="breakfast" value="breakfast" />
+            <Picker.Item label="lunch" value="lunch" />
+            <Picker.Item label="dinner" value="dinner" />
+            <Picker.Item label="snack" value="snack" />
+          </Picker>
+          <View>
+            <Text>current mood</Text>
+            <View>
+              <TouchableOpacity onPress={() => { setMood('positive'); }}>
+                <Text style={{ color: mood === 'positive' ? 'black' : 'red' }}>positive</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => { setMood('neutral') }}>
+                <Text style={{ color: mood === 'neutral' ? 'black' : 'red' }}>neutral</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => { setMood('negative') }}>
+                <Text style={{ color: mood === 'negative' ? 'black' : 'red' }}>negative</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <TouchableOpacity onPress={submitForm}>
+            <Text>submit</Text>
+          </TouchableOpacity>
+        </View>
+        )}
       </Camera>
       )}
       {!hasPermission
