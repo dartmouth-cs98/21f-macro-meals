009727274d496ea9aaa3ef507b12ce6ce46cc829
qspraut <quinn.a.spraut.22@dartmouth.edu>
2022-02-28 17:20:46 -0500

    making progress computer almost dead lol

--- macro-frontend/src/screens/BreakdownScreen.js
+++ macro-frontend/src/screens/BreakdownScreen.js
@@ -14,9 +14,44 @@ import styles from '../styles';
 const windowWidth = Dimensions.get('window').width;
 
 function BreakdownScreen({ navigation, foodList }) {
-  const food = foodList[foodList.length - 1]; // get the most recent food off of the list
+  const baseFood = foodList[foodList.length - 1];
   const [letUsKnow, setLetUsKnow] = useState(false);
-  console.log(food);
+  const [classificationNumber, setClassificationNumber] = useState(1);
+  const [food, setFood] = useState({
+    customName: baseFood.customName,
+    classification: baseFood.classification,
+    calories: baseFood.calories,
+    protein: baseFood.protein,
+    carb: baseFood.carb,
+    fat: baseFood.fat,
+    confidence: baseFood.confidence,
+  });
+  console.log(baseFood);
+
+  const updateClassification = (num) => {
+    setClassificationNumber(num);
+    if (num == 2) {
+      setFood({
+        customName: baseFood.customName,
+        classification: baseFood.classificationTwo,
+        calories: baseFood.caloriesTwo,
+        protein: baseFood.proteinTwo,
+        carb: baseFood.carbTwo,
+        fat: baseFood.fatTwo,
+        confidence: baseFood.confidenceTwo,
+      })
+    } else if (num == 3) {
+      setFood({
+        customName: baseFood.customName,
+        classification: baseFood.classificationThree,
+        calories: baseFood.caloriesThree,
+        protein: baseFood.proteinThree,
+        carb: baseFood.carbThree,
+        fat: baseFood.fatThree,
+        confidence: baseFood.confidenceThree,
+      })
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -24,7 +59,7 @@ function BreakdownScreen({ navigation, foodList }) {
         <Icon name="camera" color="white" style={{ fontSize: 0.05 * windowWidth }} />
       </TouchableOpacity>
       <Text style={[ styles.secFontBold, { fontSize: 24 }]}>breakdown</Text>
-      <MacroPieChart allMacros={foodList} />
+      <MacroPieChart allMacros={food} />
       <Text style={[ styles.secFontBold, { marginBottom: 10, fontSize: 18 } ]}>{food.customName}</Text>
       <View style={styles.centerMeEvenly}>
         <View style={styles.flexCol}><Text style={styles.secFontBold}>Protein: </Text><Text style={styles.secFont}>{food.protein}g</Text></View>
@@ -34,16 +69,9 @@ function BreakdownScreen({ navigation, foodList }) {
       <View style={styles.flexCol}><Text style={[ styles.secFontBold, { marginTop: 10} ]}>Classification: </Text><Text style={styles.secFont}>{food.classification}</Text></View>
       <View style={[ styles.flexCol, { marginBottom: 10 } ]}><Text style={styles.secFontBold}>Confidence: </Text><Text style={styles.secFont}>{(food.confidence*100).toFixed(1)}%</Text></View>
       <View>
-        <Text style={styles.secFont}>Incorrect classification?</Text>
+        <Text style={styles.secFont}>incorrect classification?</Text>
         <View>
-          {!letUsKnow
-          && (
-            <TouchableOpacity style={styles.authBtn} onPress={() => { setLetUsKnow(!letUsKnow) }}><Text style={styles.authBtnFont}>Let Us Know!</Text></TouchableOpacity>
-          )}
-          {letUsKnow
-          && (
-            <TouchableOpacity style={styles.authBtn} onPress={() => { setLetUsKnow(!letUsKnow) }}><Text style={styles.authBtnFont}>Coming soon...</Text></TouchableOpacity>
-          )}
+          <TouchableOpacity style={styles.authBtn} onPress={() => { updateClassification(classificationNumber + 1); }}><Text style={styles.authBtnFont}>next option</Text></TouchableOpacity>
         </View>
       </View>
     </View>
